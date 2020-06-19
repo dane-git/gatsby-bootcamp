12173d61f4717ce218aed62077188a811f209607
Dane M Ogden <DaneOgden1@gmail.com>
2020-06-19 15:28:45 -0500

    add sass to projet
    installed node-sass gatsby-plugin-sass,
    renamed index.css -> index.scss
    gatsby-config.js -> added module.exports { plugins: [gatsby-plugin-sass]}

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,9 @@ import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 
+// import the base style sheet to the layout component, because the layout component is used on every page.
+import "../styles/index.scss"
+
 const Layout = props => {
   return (
     <div>
